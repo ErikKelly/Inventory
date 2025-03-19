a14cfa1d59699cedc4b57286b8dc5c172ebcb668
ErikKelly <erikkelly@gmail.com>
2025-03-19 10:39:27 -0400

    config update

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,7 +1,4 @@
 export const SITE_NAME = "EK's Cards"
-// export const SHEET_ID = "1gUdbhzWjdRmcet7CX592wJ25GN_CW-k1J08wERqzAGY";
-// export const SHEET_TABS = "Graded Cards, Sets, Raw Cards";
-
-export const SHEET_ID = "14qJJYCwu5_aaS4ecJdDG7juKcEssWETr2-dicbcSGjw";
-export const SHEET_TABS = "Tab1,Tab2,Tab3";
+export const SHEET_ID = "1gUdbhzWjdRmcet7CX592wJ25GN_CW-k1J08wERqzAGY";
+export const SHEET_TABS = "Graded Cards, Sets, Raw Cards";
 export const PRIMARY_COLOR = 'rgb(75, 31, 31)';
